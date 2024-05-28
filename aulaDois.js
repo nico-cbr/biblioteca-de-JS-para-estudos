5434a4a97756da63172893d7f34a8f76adfad650
Nicoly <153707458+nico-cbr@users.noreply.github.com>
2024-05-28 10:48:51 -0300

    Add files via upload

---
+++ aulaDois.js
@@ -0,0 +1,12 @@
+// variaveis parte dois
+var altura = 5;
+var comprimento = 10;
+area = altura * comprimento;
+var area;
+console.log(area)
+
+let alturaUm = 5;
+let comprimentoDois = 10;
+areaT = alturaUm * comprimentoDois;
+let areaT;
+console.log(area)
